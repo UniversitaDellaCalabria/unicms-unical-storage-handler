a5c7fb0120b264285082f7b2220812294597165d
Francesco Filicetti <francesco.filicetti@unical.it>
2024-02-29 18:03:46 +0100

    fix: cds website prospect link to website

--- src/unicms_unical_storage_handler/templatetags/unicms_storage_handler.py
+++ src/unicms_unical_storage_handler/templatetags/unicms_storage_handler.py
@@ -100,23 +100,11 @@ def get_cds_website_cds_cod(value=None, cds_cod=None):
 
 @register.simple_tag
 def get_cds_website_root_path(webpath=None, cds_cod=None):
-    if not webpath: return None
     if not cds_cod: return None
     cms_webpath_cds = getattr(settings, 'CMS_WEBPATH_CDS', {})
     if not cms_webpath_cds: return None
-    if cms_webpath_cds.get(webpath.pk) == cds_cod:
+    if webpath and cms_webpath_cds.get(webpath.pk) == cds_cod:
         return webpath
-    for k,v in cms_webpath_cds.items():
-        if v == cds_cod:
-            return WebPath.objects.get(pk=k, is_active=True)
-    return None
-
-
-@register.simple_tag
-def get_cds_website_path_pk(cds_cod=None):
-    if not cds_cod: return None
-    cms_webpath_cds = getattr(settings, 'CMS_WEBPATH_CDS', {})
-    if not cms_webpath_cds: return None
     for k,v in cms_webpath_cds.items():
         if v == cds_cod:
             return WebPath.objects.get(pk=k, is_active=True)
